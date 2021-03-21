65aa3fc55eb40ba1532fda97d905e6b660501be6
CharlesOB <48743701+CharlesOB@users.noreply.github.com>
2021-03-21 15:39:03 -0700

    Add file to launch the game
    Run ```java Play``` to launch the game.

---
+++ Play.java
@@ -0,0 +1,11 @@
+import ui.UBotWindow;
+import javax.swing.JFrame;
+
+public class Play {
+
+  public static void main (String[] args) {
+    JFrame frame = new UBotWindow();
+    frame.setVisible(true);
+  }
+
+}
